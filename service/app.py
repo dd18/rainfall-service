b3c8c0a1186cf3d78b255323e0d3809b89836db5
dd <debit2005@gmail.com>
2022-05-29 14:09:58 +0530

    Code added to pass the first unit test

---
+++ service/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from gevent.pywsgi import WSGIServer
+
+app=Flask(__name__)
+@app.route('/')
+def query_rainfall():
+    return "Raining"
+
+if __name__=='__main__':
+    http_server = WSGIServer(('',8080),app)
+    http_server.serve_forever()
