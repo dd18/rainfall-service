b2bef7c390aed7b2a9d2e1740f5bd3bfc9ad8e51
dd <debit2005@gmail.com>
2022-05-29 16:11:33 +0530

    input config file reading code added

--- service/app.py
+++ service/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from gevent.pywsgi import WSGIServer
+import yaml
 
 app=Flask(__name__)
 @app.route('/')
 def query_rainfall():
     return "Raining"
+
+def read_config(file_name):
+    with open(file_name, 'r') as file:
+        load_config = yaml.safe_load(file)
+        url=load_config['url']
+        loc=load_config["location"]
+    return url,loc
 
 if __name__=='__main__':
     http_server = WSGIServer(('',8080),app)
