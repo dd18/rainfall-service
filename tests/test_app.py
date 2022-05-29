0072f2b7b17cd85ab6f2923ada7158c2b004f62c
dd <debit2005@gmail.com>
2022-05-29 17:24:14 +0530

    rain fall check test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from service.app import app,read_config
+from service.app import app,read_config,check_rainfall
 
 def test_app_status_code():
     tester = app.test_client()
@@ -10,3 +10,7 @@ def test_load_config_file():
     url,loc=read_config('conf/config.yaml')
     assert url == 'https://api.data.gov.sg/v1/environment/rainfall'
     assert loc == 'Kent Ridge Road'
+
+def test_check_rainfall():
+    output=check_rainfall('https://api.data.gov.sg/v1/environment/rainfall','Kent Ridge Road')
+    assert 'Kent Ridge Road' in output
