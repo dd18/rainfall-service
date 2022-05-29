ae90a639ea7673eea825769e8afb09e16054a61e
dd <debit2005@gmail.com>
2022-05-29 16:05:55 +0530

    input config file read unit test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,12 @@
-from service.app import app
+from service.app import app,read_config
 
 def test_app_status_code():
     tester = app.test_client()
     response = tester.get("/", content_type="html/text")
 
     assert response.status_code == 200
+
+def test_load_config_file():
+    url,loc=read_config('conf/config.yaml')
+    assert url == 'https://api.data.gov.sg/v1/environment/rainfall'
+    assert loc == 'Kent Ridge Road'
