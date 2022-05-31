1e2f7988e3f4e823b0a688e83e2390477d714542
dd18 <36710070+dd18@users.noreply.github.com>
2022-05-31 10:31:07 +0530

    Update app.py

--- service/app.py
+++ service/app.py
@@ -46,7 +46,7 @@ def check_rainfall(url,loc):
         output="There is some internal error. Please contact the admin"
     return output
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def query_rainfall():
     try:
         url,loc=read_config("conf/config.yaml")
